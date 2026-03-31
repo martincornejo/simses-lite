7dc982f184e85908313ed3dcf6f26767e5ebe256
Martin Cornejo <martin.cornejo@tum.de>
2026-03-31 22:07:07 +0200

    Export DegradationState

--- simses/degradation/__init__.py
+++ simses/degradation/__init__.py
@@ -2,11 +2,13 @@ from simses.degradation.calendar import CalendarDegradation
 from simses.degradation.cycle_detector import HalfCycle, HalfCycleDetector
 from simses.degradation.cyclic import CyclicDegradation
 from simses.degradation.degradation import DegradationModel
+from simses.degradation.state import DegradationState
 
 __all__ = [
     "CalendarDegradation",
     "CyclicDegradation",
     "DegradationModel",
+    "DegradationState",
     "HalfCycle",
     "HalfCycleDetector",
 ]
