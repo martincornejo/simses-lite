100dd9c4e15f2cc15897d91a434725874112a480
Martin Cornejo <martin.cornejo@tum.de>
2026-04-16 18:20:11 +0200

    Export cell formats

--- src/simses/battery/__init__.py
+++ src/simses/battery/__init__.py
@@ -1,12 +1,18 @@
 from simses.battery.battery import Battery
 from simses.battery.derating import CurrentDerating, DeratingChain, LinearThermalDerating, LinearVoltageDerating
+from simses.battery.format import CellFormat, PrismaticCell, RoundCell, RoundCell18650, RoundCell26650
 from simses.battery.state import BatteryState
 
 __all__ = [
     "Battery",
     "BatteryState",
+    "CellFormat",
     "CurrentDerating",
     "DeratingChain",
     "LinearThermalDerating",
     "LinearVoltageDerating",
+    "PrismaticCell",
+    "RoundCell",
+    "RoundCell18650",
+    "RoundCell26650",
 ]
